3a51068adda1da923c4641ed64dadc2156fb841c
Zoli <Zoli@DESKTOP-748KGQ5>
2023-10-24 20:42:06 +0200

    Add project files.

---
+++ CubeSolver/cube_solver.cuh
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string.h>
+
+#include "cube_rotations.cuh"
+#include "cube_printer.cuh"
+#include "ms_clock.cuh"
+
+extern void solve(const int cube[3][9][6]);
---
+++ CubeSolver/kernel.cu
@@ -0,0 +1,47 @@
+﻿#include "cube_solver.cuh"
+#include "variables.cuh"
+#include "sequence_processor.cuh"
+#include "notation_translator.cuh"
+#include "test_rotations.cuh"
+
+int main()
+{
+    cudaSetDeviceFlags(cudaDeviceScheduleSpin);
+    long long start = timeInMilliseconds();
+    solve(testCubeColors);
+    findSequence();
+    Notation sequence[263]{};
+    cudaMemcpyFromSymbol(sequence, dev_sequence, sizeof(sequence));
+    long long end = timeInMilliseconds();
+    printf("%d\n", end - start);
+    for (int i = 0; i < 263; i++)
+    {
+        if (sequence[i] != None)
+        {
+            const TranslatedNotation move = translateNotation(sequence[i]);
+            if (move.layer)
+            {
+                turnTestLayer(move.cubeLayer, move.direction, move.twice);
+            }
+            else
+            {
+                turnTestCube(move.direction, move.twice);
+            }
+            printf("%d ", sequence[i]);
+        }
+    }
+    Color testColors[3][9][6]{};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            for (int k = 0; k < 6; k++)
+            {
+                testColors[i][j][k] = (Color)testCubeColors[i][j][k];
+            }
+        }
+    }
+    printf("\n");
+    printCube(testColors);
+    scanf("%s");
+}
